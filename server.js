caac2b7125559f800b4e3928cdf79f041e8bf8aa
maikaka <maikakaidiaboubacar@gmail.com>
2026-03-26 20:32:22 +0100

    Fix CORS + client offline/online sync

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const app = express();
 /* ================= MIDDLEWARE ================= */
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+
 app.use(cors({
   origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE"],
